b3db778cd607a58d675865a7baf5992677e9681f
Johan Sannemo <johan.sannemo@gmail.com>
2014-08-31 22:45:03 +0200

    Add more testdata and partial solutions to "apples"

--- onlinekval/apples/data/gen_random.sh
+++ onlinekval/apples/data/gen_random.sh
@@ -1,3 +1,6 @@
+./gen 45234 5 5 1 > secret/same_group_50/random_5_5.in
+./gen 35474 5 10 1 > secret/same_group_50/random_5_10.in
+./gen 81240 5 15 1 > secret/same_group_50/random_5_15.in
 ./gen 92837 10 5 1 > secret/same_group_50/random_10_5.in
 ./gen 12354 10 10 1 > secret/same_group_50/random_10_10.in
 ./gen 28953 10 15 1 > secret/same_group_50/random_10_15.in
@@ -6,9 +9,20 @@
 ./gen 91754 15 15 1 > secret/same_group_50/random_15_15.in
 
 
+./gen 45234 5 5 0 > secret/different_group_50/random_5_5.in
+./gen 35474 5 10 0 > secret/different_group_50/random_5_10.in
+./gen 81240 5 15 0 > secret/different_group_50/random_5_15.in
 ./gen 92837 10 5 0 > secret/different_group_50/random_10_5.in
 ./gen 12354 10 10 0 > secret/different_group_50/random_10_10.in
 ./gen 28953 10 15 0 > secret/different_group_50/random_10_15.in
 ./gen 23435 15 5 0 > secret/different_group_50/random_15_5.in
 ./gen 24584 15 10 0 > secret/different_group_50/random_15_10.in
 ./gen 91754 15 15 0 > secret/different_group_50/random_15_15.in
+
+
+g++ ../submissions/accepted/apples_eo.cpp -o sol
+
+for x in $(find | grep in$)
+do
+	./sol < $x > ${x%.in}.ans
+done
--- onlinekval/apples/input_format_validators/validator.py
+++ onlinekval/apples/input_format_validators/validator.py
@@ -18,4 +18,7 @@ for i in range(2):
     assert len(nums) == n # check that each line have exactly n numbers
     assert len([x for x in nums if x > 1000 or x < 0]) == 0 # check that all numbers are <= 1000
 
+line = sys.stdin.readline()
+assert len(line) == 0
+
 sys.exit(42)
