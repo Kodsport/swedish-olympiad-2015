26c869312659d948c1a72b8ca52c70bb4fb740ce
EmanuelGedin <egedin@kth.se>
2014-10-16 18:23:21 +0200

    First Draft

---
+++ skolkval/kentbuggen/input_format_validators/validator.py
@@ -0,0 +1,24 @@
+#/usr/bin/env python
+
+import re
+import sys
+
+re_int = "^[1-9][0-9]*$"
+re_string = "^[a-z]*$"
+
+data = []
+
+for line in sys.stdin:
+	if line.strip() != "": data.append(line.strip())
+
+n = int(data[0])
+assert re.match(re_int, data[0])
+assert n <= 10**5	
+
+assert len(data) == n+1	
+
+for i in range(1, n+1):
+	assert re.match(re_string, data[i])
+	assert len(data[i]) <= 20
+
+sys.exit(42)
---
+++ skolkval/kentbuggen/submissions/accepted/emanuel.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+typedef long long LL;
+typedef long double LD;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+#define rep(i, a) for(int i = 0; i < (a); i++)
+#define foru(i, s, e) for(int i = (s); i < (e); i++)
+#define ford(i, s, e) for(int i = (s); i >= (e); i--)
+#define cinNewLine cin.ignore(numeric_limits<streamsize>::max(), '\n');
+const double EPS = 1e-9;
+const int INF = 2147483647;
+const LL LLINF = 9223372036854775807LL;
+template <class T> T gcd(T a, T b) { if(b == 0) return a; return gcd(b, a%b); }
+template <class T> T lcm(T a, T b) { return (a*b)/gcd(a, b); }
+
+int main() {
+	double n, m;
+	cin >> n >> m;
+	if(m <= 1) cout << "Det här gick ju inte bra, inte bra alls" << endl;
+	else {
+		n /= 60.0;
+		double max = 60/( n * (m-1) );
+		double min = 60/( n * (m+1) );
+		printf("%.12f %.12f\n", min, max);
+	}
+	return 0;
+}
+
