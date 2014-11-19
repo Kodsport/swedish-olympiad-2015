96d265465202b57431ca1b9de118b41ee4b81404
Arash Rouhani <miffoljud@gmail.com>
2014-11-19 23:20:13 +0100

    kaninhol: solution
    This solution is also like an input format validator, as it will print
    all solutions, checking that there will only be one.

---
+++ skolkval/kaninhol/submissions/accepted/arash_ac.py
@@ -0,0 +1,22 @@
+import sys
+import itertools
+
+line = sys.stdin.readline().strip()
+n = int(line)
+line = sys.stdin.readline().strip()
+end_position = map(int, line.split())
+
+start_position = range(n, 0, -1)
+
+def enter_hole(xs, hole_size):
+  r = xs[0:-hole_size]
+  l = list(reversed(xs[-hole_size:]))
+  return l + r
+
+perms = itertools.product(range(1, n), repeat=3)
+for hss in perms:
+  position = start_position
+  for hs in hss:
+    position = enter_hole(position, hs)
+  if position == end_position:
+    print("%s %s %s" % hss)
