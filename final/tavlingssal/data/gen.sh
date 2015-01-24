c0734d84c4330f52b8cc85f932e986b6e51d41b5
EmanuelGedin <egedin@kth.se>
2015-01-24 19:09:23 +0100

    Added tricky testcase
    Added the following testcases:
    997^2 + 1
    997^2
    29989^2 + 1
    29989^2

--- final/tavlingssal/data/gen.sh
+++ final/tavlingssal/data/gen.sh
@@ -43,14 +43,18 @@ for i in {1..10}
 do
 	python gen_testcase.py 1001 1000000 $i > secret/g2/$PROBLEMNAME.g2.$i.in
 done
-python gen_testcase.py 1000000 1000000 0 > secret/g2/$PROBLEMNAME.g2.11.in
+python gen_testcase.py 994010 994010 0 > secret/g2/$PROBLEMNAME.g2.11.in
+python gen_testcase.py 994009 994009 0 > secret/g2/$PROBLEMNAME.g2.12.in
+python gen_testcase.py 1000000 1000000 0 > secret/g2/$PROBLEMNAME.g2.13.in
 
 echo "Generating group 3..."
 for i in {1..10}
 do
 	python gen_testcase.py 1000001 1000000000 $i > secret/g3/$PROBLEMNAME.g3.$i.in
 done
-python gen_testcase.py 1000000000 1000000000 0 > secret/g3/$PROBLEMNAME.g3.11.in
+python gen_testcase.py 899340122 899340122 0 > secret/g3/$PROBLEMNAME.g3.11.in
+python gen_testcase.py 899340121 899340121 0 > secret/g3/$PROBLEMNAME.g3.12.in
+python gen_testcase.py 1000000000 1000000000 0 > secret/g3/$PROBLEMNAME.g3.13.in
 
 # generate solutions for all files
 if [[ ! -z $SOLVER ]]
