52646586033b225cf7a7bc4637173ff22a39e697
Arash Rouhani <miffoljud@gmail.com>
2015-01-25 16:56:04 +0100

    Vandrarhem inputs and solutions

--- final/vandrarhem/input_format_validators/validator.py
+++ final/vandrarhem/input_format_validators/validator.py
@@ -1,26 +1,28 @@
 #/usr/bin/env python
 
-# kentbuggen, ska fixa for vandrarhem
-
 import re
 import sys
 
 re_int = "^[1-9][0-9]*$"
-re_string = "^[a-z]*$"
 
 data = []
 
 for line in sys.stdin:
-	if line.strip() != "": data.append(line.strip())
+    if line.strip() != "": data.append(line.strip())
 
-n = int(data[0])
-assert re.match(re_int, data[0])
-assert n <= 10**5	
+N, R = map(int(data[0].split()))
+assert 1 <= N <= 100
+assert 1 <= R <= 5
 
-assert len(data) == n+1	
+assert len(data) == R+1
 
-for i in range(1, n+1):
-	assert re.match(re_string, data[i])
-	assert len(data[i]) <= 20
+sum_r = 0
+for i in range(1, R+1):
+    c, r = map(int(data[i].split()))
+    assert 100 <= c <= 100
+    assert 1   <= r <= 100
+    sum_r += r
+
+assert N <= sum_r
 
 sys.exit(42)
---
+++ final/vandrarhem/submissions/wrong_answer/arash_wa.py
@@ -0,0 +1,18 @@
+#/usr/bin/env python
+
+import sys
+data = []
+
+for line in sys.stdin:
+    if line.strip() != "": data.append(line.strip())
+
+N, R = map(int, data[0].split())
+l = []
+
+for i in range(1, R+1):
+    c, r = map(int, data[i].split())
+    l += r*[c]
+
+# l = sorted(l) # comment out for WA
+
+print(sum(l[0:N]))
