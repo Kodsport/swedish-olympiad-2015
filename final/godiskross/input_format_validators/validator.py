cf846fa6430c408a2b19d2b30711a8524b457601
Par Soderhjelm <par.soderhjelm@teokem.lu.se>
2015-01-28 19:35:05 +0100

    godiskross: fixes in scoring and validator

--- final/godiskross/input_format_validators/validator.py
+++ final/godiskross/input_format_validators/validator.py
@@ -5,4 +5,18 @@ int_regex = "(0|[1-9][0-9]*)"
 intspace_regex = int_regex + "( " + int_regex+")*"
 line_regex = "^" + intspace_regex + "$"
 
+line=sys.stdin.readline()
+assert re.match(line_regex, line)
+assert len(line.split())==1
+N=int(line)
+assert 1<=N<=5
+
+line=sys.stdin.readline()
+assert re.match("^([ABC]*)$",line)
+org=line.strip()
+R=len(org)
+assert 3<=R<=20
+for i in range(2,R):
+    assert not (org[i]==org[i-1] and org[i]==org[i-2])
+
 sys.exit(42)
