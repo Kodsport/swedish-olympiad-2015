654b1e30c647e42a74ef90c9846ac338e80c0de2
Oskar Werkelin Ahlin <osk@po.scrool.se>
2015-01-29 09:40:54 +0100

    vandrarhem: solutions

---
+++ final/vandrarhem/submissions/wrong_answer/osk_unsorted.cc
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<pair<int,int>> v;
+    for (int i = 0; i < M; ++i) {
+        int ci, bi;
+        cin >> ci >> bi;
+        v.emplace_back(ci, bi);
+    }
+    // sort(v.begin(), v.end());
+    int res = 0;
+    for (const auto p : v) {
+        const int buy_here = N >= p.second ? p.second : N;
+        N -= buy_here;
+        res += p.first * buy_here;
+    }
+    cout << res << endl;
+}
