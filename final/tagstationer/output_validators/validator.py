cf479db2a369e677d2155cc79eb72f2d231a1463
Erik Odenman <erikko@spotify.com>
2015-01-29 20:12:56 +0100

    tagstationer: added solution and output validator
    Change-Id: I9483226f6549472cf1dde84e1ace7a2b8bca18e3

---
+++ final/tagstationer/output_validators/validator.py
@@ -0,0 +1,38 @@
+import sys
+
+indata = open(sys.argv[1], 'r')
+
+N = int(indata.readline())
+stations = []
+for i in range(N):
+  stations.append(map(int, indata.readline().split()))
+
+f = open(sys.argv[2], 'r')
+judge_answer = f.readline().strip()
+answer = sys.stdin.readline().strip()
+
+if answer != judge_answer:
+  exit(43)
+
+try:
+  if answer == "JA":
+    order = map(int,sys.stdin.readline().strip().split())
+    if len(order) != N:
+      exit(43)
+    used = [False for i in order]
+    curr = 0
+    for station in order:
+      station -= 1
+      if station < 0 or station >= N or used[station]:
+        exit(43)
+      used[station] = True
+      if stations[station][1] > curr:
+        exit(43)
+      curr -= stations[station][1]
+      curr += stations[station][0]
+    if curr != 0:
+      exit(43)
+except Exception:
+  exit(43)
+
+exit(42)
---
+++ final/tagstationer/submissions/accepted/eo100.py
@@ -0,0 +1,38 @@
+import sys
+
+N = int(sys.stdin.readline())
+plus = []
+minus = []
+for i in range(N):
+  on,off = map(int, sys.stdin.readline().split())
+  if off < on:
+    plus.append((off,on,i+1))
+  else:
+    minus.append((on,off,i+1))
+
+plus = sorted(plus)
+minus = sorted(minus, reverse=True)
+
+good = True
+curr = 0
+order = []
+for train in plus:
+  if train[0] > curr:
+    good = False
+  curr -= train[0]
+  curr += train[1]
+  order.append(train[2])
+
+for train in minus:
+  if train[1] > curr:
+    good = False
+  curr -= train[1]
+  curr += train[0]
+  order.append(train[2])
+
+good = good and curr == 0
+if good:
+  print "JA"
+  print " ".join(map(str,order))
+else:
+  print "NEJ"
