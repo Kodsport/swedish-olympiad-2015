0f99e6929ac692e5080b30997d06c175ec196ca0
EmanuelGedin <egedin@kth.se>
2015-03-11 20:42:25 +0100

    Update validator.py

--- katt/kohagen/input_format_validators/validator.py
+++ katt/kohagen/input_format_validators/validator.py
@@ -1,26 +1,5 @@
 #/usr/bin/env python
 
-# kentbuggen, ska fixa for vandrarhem
-
-import re
-import sys
-
-re_int = "^[1-9][0-9]*$"
-re_string = "^[a-z]*$"
-
-data = []
-
-for line in sys.stdin:
-	if line.strip() != "": data.append(line.strip())
-
-n = int(data[0])
-assert re.match(re_int, data[0])
-assert n <= 10**5	
-
-assert len(data) == n+1	
-
-for i in range(1, n+1):
-	assert re.match(re_string, data[i])
-	assert len(data[i]) <= 20
+# TODO
 
 sys.exit(42)
