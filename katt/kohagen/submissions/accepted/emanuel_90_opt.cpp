32818c7cdf1121c64179e8a262f3d676d19cece4
Simon Lindholm <simon.lindholm10@gmail.com>
2015-03-12 21:44:15 +0100

    kohagen: make emanuel_90 solution faster

--- katt/kohagen/submissions/accepted/emanuel_90_opt.cpp
+++ katt/kohagen/submissions/accepted/emanuel_90_opt.cpp
@@ -3,69 +3,68 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define rep(i, a, b) for(int i = a; i < int(b); i++)
 #define trav(it, v) for(auto it = v.begin(); it != v.end(); it++)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int) (x).size()
- 
+
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
-typedef vector<vector<int>> vvi; 
- 
-vector<double> a, si, co;
-int n;
-double m, r;
+typedef vector<vector<int>> vvi;
 
-double getArea(int i, int j, int k) {
-	double x1 = r*co[i];
-	double y1 = r*si[i];
-	double x2 = r*co[j];
-	double y2 = r*si[j];
-	double x3 = r*co[k];
-	double y3 = r*si[k];
-	return abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2));
-}
+vector<float> a, si, co;
+int n;
+float m, r;
 
 int main() {
-	scanf("%d %lf %lf", &n, &m, &r);
-	m *= 2.0;
+	ignore = scanf("%d %f %f", &n, &m, &r);
+	m *= 2;
 	a.resize(n);
 	co.resize(n);
 	si.resize(n);
 	rep(i, 0, n) {
-		scanf("%lf", &a[i]);
-		a[i] = a[i] * M_PI / 180.0;
+		ignore = scanf("%f", &a[i]);
+		a[i] = a[i] * (float)M_PI / 180;
 	}
 	sort(all(a));
 	rep(i, 0, n) {
 		co[i] = cos(a[i]);
 		si[i] = sin(a[i]);
 	}
-	double ans = 8*r*r;
-	rep(i, 0, n) {
+	float ans = 8*r*r;
+	float C, CX, CY;
+	int N = n;
+	// if (N > 100) N /= 16;
+	rep(i, 0, N) {
 		rep(j, i+2, n) {
-			vector<double> v1, v2;
+			vector<float> v1, v2;
+
+			CX = si[j] - si[i];
+			CY = co[i] - co[j];
+			C = co[j]*si[i] - co[i]*si[j];
+			C *= r*r; CX *= r*r; CY *= r*r;
 			rep(k, i+1, j) {
-				v1.push_back(getArea(i, j, k));
+				v1.push_back(CY*si[k] + CX*co[k] + C);
 			}
-			int k = j+1;
-			if(k >= n) k -= n;
-			while(k != i) {
-				v2.push_back(getArea(i, j, k));
-				k++;
-				if(k >= n) k -= n;
+
+			CX = si[i] - si[j];
+			CY = co[j] - co[i];
+			C = co[i]*si[j] - co[j]*si[i];
+			C *= r*r; CX *= r*r; CY *= r*r;
+			rep(k, j+1, n) {
+				v2.push_back(CY*si[k] + CX*co[k] + C);
 			}
 
 			sort(all(v1));
 			sort(all(v2));
 
 			// Sweep
-			k = 0;
-			int l = v2.size()-1;
-			while(k < v1.size() && l >= 0) {
-				double area = v1[k] + v2[l];
+			int k = 0;
+			int l = sz(v2)-1;
+			while(k < sz(v1) && l >= 0) {
+				float area = v1[k] + v2[l];
 				if(area < m) {
 					k++;
 				} else {
@@ -75,7 +74,6 @@ int main() {
 			}
 		}
 	}
-	if(ans < 8*r*r) printf("%.9lf\n", ans/2.0);
+	if(ans < 8*r*r) printf("%.9f\n", ans/2.0);
 	else printf("-1\n");
 }
-
