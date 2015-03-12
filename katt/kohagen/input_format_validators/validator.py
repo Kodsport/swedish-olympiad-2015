ad6fa898aec1fa0a1f89fc144b098daf9be33f0d
EmanuelGedin <egedin@kth.se>
2015-03-12 15:26:08 +0100

    Added validator

--- katt/kohagen/input_format_validators/validator.py
+++ katt/kohagen/input_format_validators/validator.py
@@ -1,5 +1,23 @@
 #/usr/bin/env python
+import sys
+import re
 
-# TODO
+re1 = '^(0|[1-9][0-9]*) (0|[1-9][0-9]*) (0|[1-9][0-9]*)$'
+line = sys.stdin.readline()	
+assert 	re.match(re1, line)
+[n, m, r] = map(int, line.split())
+assert 4 <= n <= 400
+assert 1 <= m <= 1000000000
+assert 1 <= r <= 1000
+
+re2 = '^([0-9]*\.?[0-9]*)( [0-9]*\.?[0.9]*)*$'
+line = sys.stdin.readline()
+assert re.match(re2, line)
+a = map(float, line.split())
+for e in a:
+	assert 0.0 <= e < 360.0
+
+line = sys.stdin.readline()
+assert len(line) == 0	
 
 sys.exit(42)
