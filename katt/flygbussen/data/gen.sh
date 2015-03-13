54e8473b6426893c4f5822e62dc4a58cb9855455
Simon Lindholm <simon.lindholm10@gmail.com>
2015-03-13 13:03:21 +0100

    flygbussen: add a case with large t

--- katt/flygbussen/data/gen.sh
+++ katt/flygbussen/data/gen.sh
@@ -26,10 +26,13 @@ function testcase {
 	if [[ $n -gt $maxn ]]; then echo fail; fi
 }
 
-#g++ -std=c++11 -O2 ../submissions/accepted/mw_flygbussen.cpp -o sol
-javac ../submissions/accepted/Solver.java
-mv ../submissions/accepted/Solver.class .
-SOLVER="java -Xss40m Solver"
+function testcase_large_time {
+	ind=$((ind+1))
+	cp large_time.txt secret/$groupname/$PROBLEMNAME.$groupname.$ind.in
+}
+
+g++ -std=c++11 -O2 ../submissions/accepted/solver_ported.cpp -o sol
+SOLVER=./sol
 
 # max_n, k, min_n, max_t, max_group_size, max_duplicate
 
@@ -47,12 +50,14 @@ group g2 18
 testcase 100000 1 99000 10000000 3 0
 testcase 100000 1 18000 10000000 2 10
 testcase 100000 1 70000 50000 3 1
+testcase_large_time
 
 # N, K <= 100
 group g3 19
 testcase 100 97 76 10000 10 1
 testcase 100 84 90 1000 10 0
 testcase 100 19 64 10000 10 2
+testcase_large_time
 
 # N <= 1000, K <= 100
 group g4 23
@@ -60,6 +65,7 @@ testcase 1000 55 100 100000 60 5
 testcase 1000 99 500 10000 60 2
 testcase 1000 98 979 100000 40 0
 testcase 1000 98 979 5000 40 0
+testcase_large_time
 
 # N <= 100000, K <= 100
 group g5 27
@@ -69,6 +75,7 @@ testcase 100000 100 27000 1000000 60 6
 testcase 100000 98 30001 10000 40 5
 testcase 100000 98 30000 1000000 40 5
 testcase 100000 98 98000 1000000 40 0
+testcase_large_time
 
 for i in g1 g2 g3 g4 g5
 do
