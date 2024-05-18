42469f8cabcb0f4c0508ce4bd8bf89c2f48cc744
matistjati <matistjati@outlook.com>
2024-05-18 17:41:21 +0200

    fix apples subtask order

--- onlinekval/apples/data/generator.sh
+++ onlinekval/apples/data/generator.sh
@@ -9,7 +9,7 @@ compile gen.cc
 samplegroup
 sample 1
 
-group same_group 50
+group 1-same_group 50
 limits same=yes
 tc same_5_5 gen 45234 5 5 1
 tc same_5_10 gen 35474 5 10 1
@@ -21,10 +21,10 @@ tc same_15_5 gen 23435 15 5 1
 tc same_15_10 gen 24584 15 10 1
 tc same_15_15 gen 91754 15 15 1
 
-group different_group 50
+group 2-different_group 50
 limits same=no
 tc 1
-include_group same_group
+include_group 1-same_group
 tc different_5_5 gen 45234 5 5 0
 tc different_5_10 gen 35474 5 10 0
 tc different_5_15 gen 81240 5 15 0
