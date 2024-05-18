1628b4837d2120e348338f37b464872e9fb5eada
matistjati <matistjati@outlook.com>
2024-05-18 23:13:45 +0200

    Fixed kohagen

--- katt/kohagen/input_format_validators/validator.py
+++ katt/kohagen/input_format_validators/validator.py
@@ -1,11 +1,11 @@
-#/usr/bin/env python
+#!/usr/bin/python3
 import sys
 import re
 
 re1 = '''^(0|[1-9][0-9]*) (0|[1-9][0-9]*) (0|[1-9][0-9]*)$'''
 line = sys.stdin.readline() 
 assert re.match(re1, line)
-n, m, r = map(int, line.split())
+n, m, r = list(map(int, line.split()))
 assert 4 <= n <= 400
 assert 1 <= m <= 1000000000
 assert 1 <= r <= 1000
@@ -13,7 +13,7 @@ assert 1 <= r <= 1000
 re2 = '''^([0-9]*\.?[0-9]*)( [0-9]*\.?[0-9]*)*$'''
 line = sys.stdin.readline()
 assert re.match(re2, line)
-a = map(float, line.split())
+a = list(map(float, line.split()))
 assert len(a) == n
 for e in a:
     assert 0.0 <= e < 360.0
